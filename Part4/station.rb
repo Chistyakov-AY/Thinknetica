3bfd0d68654bbc2d743a83a778f2026f17452278
Chistyakov-AY <chistyakov_ay@mail.ru>
2023-04-12 23:37:29 +0300

    Part4

--- Part4/station.rb
+++ Part4/station.rb
@@ -6,16 +6,16 @@ class Station
     @trains = []
   end
 
-    def add_train(train) # Может принимать поезда (по одному за раз).
-      trains << train
-    end
+  def add_train(train) # Может принимать поезда (по одному за раз).
+    trains << train
+  end
 
-    def send_train(train) # Может отправлять поезда (по одному за раз, при этом, поезд удаляется из списка поездов, находящихся на станции).
-      trains.delete(train)
-    end
+  def send_train(train) # Может отправлять поезда (по одному за раз, при этом, поезд удаляется из списка поездов, находящихся на станции).
+    trains.delete(train)
+  end
 
-    def trains_by_type(type)  # Может возвращать список поездов на станции по типу (см. ниже): кол-во грузовых, пассажирских
-      trains.select {|i| i.type == type}
-    end
+  def trains_by_type(type)  # Может возвращать список поездов на станции по типу (см. ниже): кол-во грузовых, пассажирских
+    trains.select {|i| i.type == type}
+  end
 
 end
