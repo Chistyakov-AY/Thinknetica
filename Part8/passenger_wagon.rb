f6e9b02ffd09f064fe5cdec21568274b227c9526
Chistyakov-AY <chistyakov_ay@mail.ru>
2023-07-21 10:14:23 +0300

    Part8.v4

--- Part8/passenger_wagon.rb
+++ Part8/passenger_wagon.rb
@@ -5,7 +5,7 @@ class PassengerWagon < Wagon
   end
   
   def take_place
-    if free_place > 0 && free_place >= used_place
+    if free_place >= used_place
       @used_place += 1
     else
       puts "В вагоне нет свободных мест!"
